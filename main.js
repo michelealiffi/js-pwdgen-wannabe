a371f930d9c1959fbe40aa38393fc8b9bf302ebe
Michele Aliffi <161091503+michelealiffi@users.noreply.github.com>
2024-04-23 13:14:24 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// document
+let currentText = document.getElementById;
+
+const userName = prompt 'Inserisci il tuo nome';
+
+const userLastName = prompt 'Inserisci il tuo cognome';
+
+const favColor = prompt 'Inserisci il tuo colore preferito';
+
+// generating  a random number
+const a = Math.random();
+
